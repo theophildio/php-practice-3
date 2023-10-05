9bd4aca755f89090f9576a049606675b22a9ffa1
Theophil Dio <dtheo.sangma@gmail.com>
2023-10-05 13:21:45 +0600

    Task 1 Modify Text added to repo

---
+++ task-1.php
@@ -0,0 +1,11 @@
+<?php
+function modifyText($text)
+{
+    $text = strtolower($text);
+    $text = str_replace("brown", "red", $text);
+    echo $text;
+}
+
+$text = "The quick brown fox jumps over the lazy dog.";
+
+modifyText($text);
